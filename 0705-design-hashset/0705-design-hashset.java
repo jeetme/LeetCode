3f33fcf9b067523090feb15eafa1ea72781229db
Jeet <jeet.findme@gmail.com>
2026-08-21 21:12:39 +0530

    Time: 13 ms (62.72%), Space: 54.2 MB (17.39%)

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+
+    boolean[] keys;
+
+    public MyHashSet() {
+        this.keys = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        this.keys[key] = true;
+    }
+    
+    public void remove(int key) {
+        this.keys[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return this.keys[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

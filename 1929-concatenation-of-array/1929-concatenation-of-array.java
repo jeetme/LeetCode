6e753c7bf296f48b3cada86efa734abed78df35b
Jeet <jeet.findme@gmail.com>
2026-08-21 21:48:55 +0530

    Time: 1 ms (97.75%), Space: 47.1 MB (56.7%)

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        int i = 0;
+        for(var num : nums) {
+            ans[i] = num;
+            ans[i + n] = num;
+            i++;
+        }
+        return ans;
+    }
+}
